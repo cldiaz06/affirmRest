b2c88ec84295f6969aca0e56151df62a06243a01
cldiaz06 <cldiaz06@gmail.com>
2019-05-29 19:12:03 -0500

    initial commit

---
+++ src/main/java/com/cldiaz/selfImprove/affirmRest/AffirmRestApplication.java
@@ -0,0 +1,13 @@
+package com.cldiaz.selfImprove.affirmRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffirmRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffirmRestApplication.class, args);
+	}
+
+}
